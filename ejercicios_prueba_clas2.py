f2aaa036a079b087dc80a181520301752abe7f6a
soriaEG <gonzaaloosoria@gmail.com>
2024-12-09 07:34:26 -0300

    prueba

---
+++ ejercicios_prueba_clas2.py
@@ -0,0 +1,36 @@
+"""solicitar al usuario que ingrese dos numeros y mistrar cual 
+de los dos numeros es menor. no considerar que ambos numeros sean igaules"""
+#num1=int(input("ingresar primer numero"))
+#num2=int(input("ingresar segundo numero"))
+
+#if num1<num2:
+#    print(f"{num1} es menor que {num2}")
+#elif num2<num1:
+#    print(f"{num2} es menor que {num1}")
+#else:
+#    print(f"{num1} es igual a {num2}")
+
+"""cree un bucle que sume y mueste los numeros del 100 al 200"""
+contador=0
+num=99
+while num<200:
+    num+=1
+    contador=num
+    
+    print(contador)
+      
+"""escribe un programa que pregunte al usuario si quiere
+finalizar la ejcucion del programa.si su respuesta es si el programa finaliza """
+#igual a cero
+#nume1=int(input("primer numero pra restar"))
+#nume2=int(input("segundo numero pra restar"))
+#resultado=nume1-nume2
+#while True:
+#    if resultado==0:
+#        print(resultado)
+#    elif resultado!=0:
+#        print("esta resta no es igual a cero")
+#    pregunta=input("quieres detener la resta?")
+#    if pregunta == "si":   
+#    print("final")
+#            break
